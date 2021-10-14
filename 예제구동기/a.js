20212bba4cf9ccd2625edf6af15c88c116eea712
Junha <mythpow@gmail.com>
2021-10-14 17:50:47 +0900

    1week draft

---
+++ 예제구동기/a.js
@@ -0,0 +1,13 @@
+let human = {
+  face: 'handsome',
+  height: 1.8,
+  weight: 80,
+};
+
+console.log(`human의 face 스펙입니다. ${human.face}`);
+
+ork = human;
+ork.face = 'beautiful';
+
+console.log(`ork의 face스펙입니다. ${ork.face}`);
+console.log(`human의 face스펙입니다. ${human.face}`);
